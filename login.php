b491510a8d7aed8fe5629af43131b4b1102fc362
unknown <njocknsem@gmail.com>
2024-08-21 17:40:43 +0100

    login functionality and logout functionality using sessions

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
-$success = 0;
-$user = 0;
+
+$login = 0;
+$invalid = 0;
 
 
 
@@ -11,40 +12,19 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
     $password = $_POST["password"];
 
 
-
-    // $sql = "INSERT INTO registration (username, password)
-    // VALUES ('$username', '$password')";
-
-
-    // if (mysqli_query($conn, $sql)) {
-    //     echo "  and New record created successfully";
-    // } else {
-    //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    // }
-    
-    // mysqli_close($conn);
-
-
-    $sql = "SELECT * FROM registration WHERE username='$username'";
+    $sql = "SELECT * FROM registration WHERE username='$username' and password='$password'";
     $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
-    $user = 1;
-    header("location:login.php");
+    $login = 1;
+    session_start();
+    $_SESSION["username"]= $username;
+    header('Location:home.php');
 
 } else {
-    $sql = "INSERT INTO registration (username, password)
-    VALUES ('$username', '$password')";
 
-    if (mysqli_query($conn, $sql)) {
-
-        $success = 1;
-        header("location:login.php");
-
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+    $invalid = 1;
 
 }
 $conn->close();
@@ -62,38 +42,38 @@ $conn->close();
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>login page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
     <body>
 
-        <!-- scrip to display the error message for existing user. -->
-        <?php 
-
-        if($user){
-            echo    '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>Sorry!!!</strong> User already exist. 
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>';
-        }
-        
-        ?>
-        <!-- scrip to display the success message for successful registration. -->
-        <?php 
-
-        if($success){
-            echo    '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong>Welcome🎉🎉</strong> registered successfully
+    <!-- script for login message when user logs in -->
+    <?php
+    if($login){
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Welcome🎉🎉</strong> Login successful. 
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>';
         }
         
         ?>
 
-        <h1 class="text-center mt-2">signup form</h1>
+    <!-- script for error message when user inputs wrong credentials -->
+    <?php
+    if($invalid){
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Sorry!!!</strong> Wrong username or password. 
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+        }
+        
+        ?>
+
+
+        <h1 class="text-center mt-2">login page</h1>
         <div class="container mt-5">
         
-            <form action="sign.php" method="post">
+            <form action="login.php" method="post">
                 <div class="row mb-3">
                     <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
                     <div class="col-sm-10">
@@ -106,7 +86,7 @@ $conn->close();
                     <input type="password" class="form-control" placeholder="Enter your password" name="password">
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary w-100">Sign up</button>
+                <button type="submit" class="btn btn-primary w-100">Sign in</button>
             </form>
         </div>
 
